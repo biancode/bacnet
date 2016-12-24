c7498b12f5d48a7300969daa12f3437d37ba57c7
amkurup <amkurup@mtu.edu>
2016-12-24 02:11:09 -0500

    Initial commit of complete bacnet project

---
+++ build/Release/.deps/Release/bacnet_stack.a.d
@@ -0,0 +1,1 @@
+cmd_Release/bacnet_stack.a := rm -rf "Release/bacnet_stack.a" && cp -af "Release/obj.target/bacnet_stack.a" "Release/bacnet_stack.a"
---
+++ build/Release/.deps/Release/obj.target/bacnet_stack/bacnet-stack/src/wp.o.d
@@ -0,0 +1,28 @@
+cmd_Release/obj.target/bacnet_stack/bacnet-stack/src/wp.o := cc '-DNODE_GYP_MODULE_NAME=bacnet_stack' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DPRINT_ENABLED=1' '-D__NO_STRING_INLINES' -I/home/akhil/.node-gyp/6.9.2/include/node -I/home/akhil/.node-gyp/6.9.2/src -I/home/akhil/.node-gyp/6.9.2/deps/uv/include -I/home/akhil/.node-gyp/6.9.2/deps/v8/include -I../bacnet-stack/include -I../bacnet-stack/demo/handler -I../bacnet-stack/ports/linux  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer  -MMD -MF ./Release/.deps/Release/obj.target/bacnet_stack/bacnet-stack/src/wp.o.d.raw   -c -o Release/obj.target/bacnet_stack/bacnet-stack/src/wp.o ../bacnet-stack/src/wp.c
+Release/obj.target/bacnet_stack/bacnet-stack/src/wp.o: \
+ ../bacnet-stack/src/wp.c ../bacnet-stack/include/bacenum.h \
+ ../bacnet-stack/include/bacdcode.h ../bacnet-stack/include/bacdef.h \
+ ../bacnet-stack/include/bacenum.h ../bacnet-stack/include/config.h \
+ ../bacnet-stack/include/datetime.h ../bacnet-stack/include/bacstr.h \
+ ../bacnet-stack/include/bacint.h ../bacnet-stack/include/bacreal.h \
+ ../bacnet-stack/include/bits.h ../bacnet-stack/include/bacdef.h \
+ ../bacnet-stack/include/wp.h ../bacnet-stack/include/bacdcode.h \
+ ../bacnet-stack/include/bacapp.h ../bacnet-stack/include/lighting.h \
+ ../bacnet-stack/include/bacdevobjpropref.h
+../bacnet-stack/src/wp.c:
+../bacnet-stack/include/bacenum.h:
+../bacnet-stack/include/bacdcode.h:
+../bacnet-stack/include/bacdef.h:
+../bacnet-stack/include/bacenum.h:
+../bacnet-stack/include/config.h:
+../bacnet-stack/include/datetime.h:
+../bacnet-stack/include/bacstr.h:
+../bacnet-stack/include/bacint.h:
+../bacnet-stack/include/bacreal.h:
+../bacnet-stack/include/bits.h:
+../bacnet-stack/include/bacdef.h:
+../bacnet-stack/include/wp.h:
+../bacnet-stack/include/bacdcode.h:
+../bacnet-stack/include/bacapp.h:
+../bacnet-stack/include/lighting.h:
+../bacnet-stack/include/bacdevobjpropref.h:
